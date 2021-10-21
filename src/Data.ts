a0a5786a9144d087e01b9d8d6686440053476f53
Kevin Frei <kevinfrei@hotmail.com>
2021-10-21 12:50:17 -0700

    Updated crypto a fair bit

--- src/Data.ts
+++ src/Data.ts
@@ -1,29 +1,31 @@
 import crypto from 'node:crypto';
 
-export type Private = { salt: string; content: string };
-
+export type Key = { iv: Buffer; key: Buffer };
 const algorithm = 'aes-256-ctr';
-const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
-const salt = crypto.randomBytes(16);
 
-export function Encrypt(text: string): Private {
-  const cipher = crypto.createCipheriv(algorithm, secretKey, salt);
-  const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-  return {
-    salt: salt.toString('hex'),
-    content: encrypted.toString('hex'),
-  };
+export function MakeKey(text: string): Key {
+  const key = crypto
+    .createHash('sha256')
+    .update(text + '-key-' + text)
+    .digest();
+  const data = crypto
+    .createHash('sha265')
+    .update('iv-' + text + '-iv')
+    .digest();
+  const vals = new Array(16).fill(0);
+  for (let i = 0; i < data.length; i++) {
+    // eslint-disable-next-line no-bitwise
+    vals[i % 16] = vals[i % 16] ^ data[i];
+  }
+  return { iv: Buffer.from(vals), key };
 }
 
-export function Decrypt(hash: Private): string {
-  const decipher = crypto.createDecipheriv(
-    algorithm,
-    secretKey,
-    Buffer.from(hash.salt, 'hex'),
-  );
-  const decrpyted = Buffer.concat([
-    decipher.update(Buffer.from(hash.content, 'hex')),
-    decipher.final(),
-  ]);
-  return decrpyted.toString();
+export function Encrypt(key: Key, data: string | Buffer): Buffer {
+  const cipher = crypto.createCipheriv(algorithm, key.key, key.iv);
+  return Buffer.concat([cipher.update(data), cipher.final()]);
+}
+
+export function Decrypt(key: Key, data: Buffer): Buffer {
+  const decipher = crypto.createDecipheriv(algorithm, key.key, key.iv);
+  return Buffer.concat([decipher.update(data), decipher.final()]);
 }
